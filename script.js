c0499d121eb8ca6308388ef2f605623183cfc9dc
jonnivar83 <58147335+jonnivar83@users.noreply.github.com>
2020-03-29 19:06:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// question 1
+var cat = {
+ complaint: function(){
+     colsole.log("meow")
+ } 
+}
+// question 2
+const heading = document.querySelector("h3");
+console.dir(heading)
+// question 3
+const heading = document.querySelector("h3")
+heading.style.padding = "2em";
+// question 4
+
+Div.heading.add("subheading")
+console.log(div.heading);
+// question 5
+
+// question 6
+const div = document.querySelector("ResultsContainer");
+console.log(div.className);
+const container = document.querySelector("ResultsContainer");
+container.innerHTML = <p>New paragraph</p>;
+// question 7
+function test1(catArray){
+console.log(catArray)}
+        
+    
+        test1();
+        const cats = [
+            {
+                name: "Blob",
+                age: 10
+            },
+            {
+                name: "Harold",
+            },
+            {
+                name: "Blurt",
+                age: 21
+            }
+        ];
+        console.log(name);
+// question 8
+Did 
+// question 9
+Not have time for more
+// question 10
